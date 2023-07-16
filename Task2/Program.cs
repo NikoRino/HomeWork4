374d89a9683b42b3623a88cd9b3aca64ee791a62
NikoRino <niko89rino@gmail.com>
2023-07-16 17:16:36 +0700

    Внес вторую задачу

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetSum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int var = number % 10;
+        sum = sum + var;
+        number = number / 10;
+    }
+    Console.WriteLine($"Oтвет: {sum}");
+}
+
+int number = GetInput("Введите число: ");
+GetSum(number);
