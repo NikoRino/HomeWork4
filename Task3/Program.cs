c076884a24268db074fef4ca9fb9bc11ff33663a
NikoRino <niko89rino@gmail.com>
2023-07-16 18:55:54 +0700

    Выполнил третье задание

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void RandomArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+    Console.WriteLine($"[{String.Join(",", array)}]");
+}
+
+int number = GetInput("Введите количество элементов массива: ");
+RandomArray(number);
