368889f67eca999b8945c4d38a7f2b6452fa2809
NikoRino <niko89rino@gmail.com>
2023-07-16 14:40:04 +0700

    Создал HM4

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetDegree(int num)
+{
+    int degree = 1;
+    for (i = 1; i <= degree; i++)
+    {
+        int num = num * i;
+    }
+    return degree;
+}
+int num = GetInput("число ");
+int degree = GetInput("степень ");
+int num = GetDegree(NumDegree);
+
+Console.WriteLine(num);
