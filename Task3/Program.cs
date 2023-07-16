ee967340478cacd60fb3521620bfbfe6c699e2ae
NikoRino <niko89rino@gmail.com>
2023-07-16 19:04:58 +0700

    Исправил третью задачу

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,15 +4,17 @@
     return Convert.ToInt32(Console.ReadLine());
 }
 
-void RandomArray(int number)
+void RandomArray(int number, int start, int end)
 {
     int[] array = new int[number];
     for (int i = 0; i < number; i++)
     {
-        array[i] = new Random().Next(100);
+        array[i] = new Random().Next(start, end);
     }
     Console.WriteLine($"[{String.Join(",", array)}]");
 }
 
 int number = GetInput("Введите количество элементов массива: ");
-RandomArray(number);
+int start = GetInput("Введите начало диапозона: ");
+int end = GetInput("Введите конец диапозона: ");
+RandomArray(number, start, end);
